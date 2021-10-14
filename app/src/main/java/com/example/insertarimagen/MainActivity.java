79448d39e4ebe6bab2c4716c7b644f2d799d5ed0
rnaranjos <83559122+rnaranjos@users.noreply.github.com>
2021-10-13 23:22:18 -0300

    Constraint

--- app/src/main/java/com/example/insertarimagen/MainActivity.java
+++ app/src/main/java/com/example/insertarimagen/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.insertarimagen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initWidgets();
+        loadImage();
+    }
+
+    private void initWidgets() {
+        imageView = findViewById(R.id.iv_fotoPerfil);
+    }
+
+    private void loadImage() {
+        Picasso.get()
+                .load("http://i.imgur.com/DvpvklR.png").into(imageView);
+
+
     }
 }
